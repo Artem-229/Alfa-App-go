f0b11809bcf7670f0240827ffaa06fe1b71af871
Artem-229 <artem321segun@gmail.com>
2025-11-13 12:22:24 +0300

    Добавлены основные функции+улулучшена структура

--- app/backend/main.go
+++ app/backend/main.go
@@ -1,15 +1,38 @@
 package main
 
 import (
-	/* "fmt" */
-	"database/sql"
-	_ "github.com/lib/pq"
-	"github.com/gin-gonic/gin"
+	"fmt"
+	//"database/sql"
+	"alfa-backend/database"
 	"log"
-	"net/http"
 
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+
+	//"log"
+	"net/http"
 )
 
 func main() {
+	r := gin.Default()
 
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal("Failed to connect")
+	}
+
+	err = database.CreateDatabase(db)
+	if err != nil {
+		log.Fatal("Failed to launch")
+	}
+
+	defer db.Close()
+
+	fmt.Println("Database created successfully")
+
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello nigga")
+	})
+
+	r.Run(":8080")
 }
